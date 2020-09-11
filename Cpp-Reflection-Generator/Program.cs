696e11b94a0e7cef31b4b03ce5590af123947f96
Paweł Krolik <pawel8krolik@gmail.com>
2020-09-11 14:53:23 +0200

    Minimal configuration for project.
    Create a Solution with two projects:
    Cpp-Reflection-Generator - main project.
    Cpp-Reflection-Generator-Tests - test project for main project.
    
    Add System.Xml.ReaderWriter NuGet package to project.

---
+++ Cpp-Reflection-Generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cpp_Reflection_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
