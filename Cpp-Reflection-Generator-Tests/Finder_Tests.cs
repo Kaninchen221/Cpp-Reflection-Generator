04ff8aec633aef149aeb53a27a857331c480d1c2
Paweł Krolik <pawel8krolik@gmail.com>
2020-09-12 12:22:18 +0200

    Add Finder class with tests.
    Finder class is used to find correct files in Doxygen xml output, contains parsed classes and
    structs.

---
+++ Cpp-Reflection-Generator-Tests/Finder_Tests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cpp_Reflection_Generator_Tests
+{
+    using Cpp_Reflection_Generator;
+
+    [TestClass]
+    public class Finder_Tests
+    {
+
+        [TestMethod]
+        public void FindXmlFilesWithParsedClassesAndStructs_Test()
+        {
+            Doxygen DoxygenObj = new Doxygen();
+            try
+            {
+                DoxygenObj.PathsOfInputFolders = Path.GetFullPath($"{Directory.GetCurrentDirectory()}\\..\\..\\..\\test_input");
+                DoxygenObj.PathToDoxyfile = "../../../../Doxyfile";
+                DoxygenObj.PrepareDoxyfile();
+                DoxygenObj.Run();
+            }
+            catch(Exception Ex)
+            {
+                Assert.Fail($"Doxygen functions shouldn't throw any exceptions : {Ex.Message}");
+            }
+
+            try
+            {
+                var FoundedFilesResult = Finder.FindXmlFilesWithParsedClassesAndStructs(DoxygenObj.XmlOutputDirectory);
+                var FoundedFilesExpectedCount = 2;
+
+                Assert.AreEqual(FoundedFilesExpectedCount, FoundedFilesResult.Count);
+            }
+            catch(Exception Ex)
+            {
+                Assert.Fail($"Finder.FindXmlFilesWithParsedClassesAndStructs function shouldn't throw any exceptions : {Ex.Message}");
+            }
+        }
+
+    }
+}
---
+++ Cpp-Reflection-Generator/Finder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Cpp_Reflection_Generator
+{
+    static public class Finder
+    {
+        public static List<string> FindXmlFilesWithParsedClassesAndStructs(string PathToFolderWithXmlFiles)
+        {
+            var StructsPaths = Directory.GetFiles(PathToFolderWithXmlFiles, "struct*.xml", SearchOption.TopDirectoryOnly);
+            var ClassesPaths = Directory.GetFiles(PathToFolderWithXmlFiles, "class*.xml", SearchOption.TopDirectoryOnly);
+
+            var Paths = StructsPaths.Concat(ClassesPaths);
+
+            return Paths.ToList();
+        }
+
+    }
+}
