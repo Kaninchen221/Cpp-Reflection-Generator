51e8fa4b3549782981cff2289f9e40117723978a
Paweł Krolik <pawel8krolik@gmail.com>
2020-09-12 08:32:32 +0200

    Add class to control Doxygen application for generate parsing info.
    Include a file contains tests.

---
+++ Cpp-Reflection-Generator-Tests/Doxygen_Tests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cpp_Reflection_Generator_Tests
+{
+    using Cpp_Reflection_Generator;
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    [TestClass]
+    public class Doxygen_Tests
+    {
+        private string WorkingDirectory = Directory.GetCurrentDirectory();
+
+        [TestMethod]
+        public void Run_Test()
+        {
+            var Doxygen = PrepareDoxygenObject();
+
+            Doxygen.PrepareDoxyfile();
+
+            try
+            {
+                Doxygen.Run();
+            }
+            catch (Exception Ex)
+            {
+                Assert.Fail($"Doxygen.Run shouldn't throw any exception : {Ex.Message}");
+            }
+        }
+
+        [TestMethod]
+        public void PrepareDoxyfile_Test()
+        {
+            var Doxygen = PrepareDoxygenObject();
+
+            try
+            {
+                Doxygen.PrepareDoxyfile();
+            }
+            catch (Exception Ex)
+            {
+                Assert.Fail($"Doxygen.PrepareDoxyfile shouldn't throw any exception : {Ex.Message}");
+            }
+
+            Assert.IsTrue(File.Exists(Doxygen.PathToTemporaryDoxyfile));
+
+        }
+
+        Doxygen PrepareDoxygenObject()
+        {
+            var Doxygen = new Doxygen();
+            Doxygen.PathToTemporaryDoxyfile = "Doxyfile";
+            Doxygen.PathsOfInputFolders = Path.GetFullPath($"{WorkingDirectory}\\..\\..\\..\\test_input");
+            Doxygen.PathToDoxyfile = "../../../../Doxyfile";
+            return Doxygen;
+        }
+    }
+}
---
+++ Cpp-Reflection-Generator/Doxygen.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Cpp_Reflection_Generator
+{
+    public class Doxygen
+    {
+        public string PathsOfInputFolders { get; set; }
+        public string PathToDoxyfile { get; set; }
+        public string PathToTemporaryDoxyfile { get; set; }
+        public string IncludePathDoxyfileProperty { get; }
+        public string DoxygenName { get; set; }
+        public string XmlOutputDirectory { get; }
+
+        public Doxygen()
+        {
+            IncludePathDoxyfileProperty = "INPUT = ";
+            DoxygenName = "doxygen";
+            XmlOutputDirectory = "xml";
+        }
+
+        public void Run()
+        {
+            RemoveXmlOutputFolder();
+
+            var DoxygenProcess = GenerateProcess();
+
+            RegisterProcessEvents(ref DoxygenProcess);
+            StartProcess(ref DoxygenProcess);
+            WaitForExitOfProcess(ref DoxygenProcess);
+
+        }
+
+        public Process GenerateProcess()
+        {
+            Process DoxygenProcess = new Process();
+            DoxygenProcess.StartInfo.FileName = DoxygenName;
+            DoxygenProcess.StartInfo.Arguments = "Doxyfile";
+
+            return DoxygenProcess;
+        }
+        
+        private void StartProcess(ref Process Process)
+        {
+            try
+            {
+                if (!Process.Start())
+                    throw new Exception("The process already started");
+            }
+            catch (Exception Ex)
+            {
+                throw new Exception($"Can't start Doxygen process. Process.Start throw exception with message: {Ex.Message}");
+            }
+        }
+
+        private void WaitForExitOfProcess(ref Process Process)
+        {
+            try
+            {
+                Process.WaitForExit();
+            }
+            catch (Exception Ex)
+            {
+                throw new Exception($"Can't wait for doxygen process exit. Process.WaitForExit throw exception with message: {Ex.Message}");
+            }
+        }
+
+        private void RegisterProcessEvents(ref Process Process)
+        {
+            Process.ErrorDataReceived += DataReceivedEventHandler;
+        }
+
+        private void DataReceivedEventHandler(object sender, DataReceivedEventArgs Args) 
+        {
+            throw new Exception($"Received an error from Doxygen Process : {Args.Data}");
+        }
+
+        public void PrepareDoxyfile()
+        {
+            bool Overwrite = true;
+
+            File.Copy(PathToDoxyfile, PathToTemporaryDoxyfile, Overwrite);
+
+            AddInputPathsToTemporaryDoxyfile();
+        }
+
+        private void AddInputPathsToTemporaryDoxyfile()
+        {
+            IfNullThenThrowException(PathsOfInputFolders);
+            IfNullThenThrowException(IncludePathDoxyfileProperty);
+
+            using (var Doxyfile = File.AppendText(PathToTemporaryDoxyfile))
+            {
+                Doxyfile.Write('\n');
+                Doxyfile.Write(IncludePathDoxyfileProperty);
+                Doxyfile.Write(PathsOfInputFolders);
+                Doxyfile.Write('\n');
+            }
+        }
+
+        private void RemoveXmlOutputFolder()
+        {
+            bool Recursive = true;
+
+            try 
+            {
+                Directory.Delete(XmlOutputDirectory, Recursive);
+            }
+            catch(Exception Ex)
+            {
+                Console.WriteLine($"Can't clear Xml output directory : {Ex.Message}");
+            }
+        }
+
+        private void IfNullThenThrowException(object Object)
+        {
+            if (Object == null)
+                throw new ArgumentNullException("Argument is null");
+        }
+
+    }
+}
