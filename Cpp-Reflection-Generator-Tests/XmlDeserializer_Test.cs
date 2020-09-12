d50abe3a507ebf3b059642e8cafc496ab20b8755
Paweł Krolik <pawel8krolik@gmail.com>
2020-09-12 12:30:11 +0200

    Test file with tests for XmlDeserializer

---
+++ Cpp-Reflection-Generator-Tests/XmlDeserializer_Test.cs
@@ -0,0 +1,52 @@
+﻿using Cpp_Reflection_Generator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Cpp_Reflection_Generator_Tests
+{
+    using Cpp_Reflection_Generator;
+
+    [TestClass]
+    public class XmlDeserializer_Test
+    {
+        [TestMethod]
+        public void DeserializeFile_DeserializeXmlOututFromDoxygenTest()
+        {
+            Doxygen DoxygenObj = new Doxygen();
+            var FinderResult = new List<string>();
+            try
+            {
+                DoxygenObj.PathsOfInputFolders = Path.GetFullPath($"{Directory.GetCurrentDirectory()}\\..\\..\\..\\test_input");
+                DoxygenObj.PathToDoxyfile = "../../../../Doxyfile";
+                DoxygenObj.PrepareDoxyfile();
+                DoxygenObj.Run();
+                FinderResult = Finder.FindXmlFilesWithParsedClassesAndStructs(DoxygenObj.XmlOutputDirectory);
+            }
+            catch (Exception Ex)
+            {
+                Assert.Fail($"Doxygen or Finder function(s) shouldn't throw any exceptions : {Ex.Message}");
+            }
+
+            var Result = new List<Cpp_Reflection_Generator.ReflectionTypes.File>();
+            try
+            {
+                foreach(var FilePath in FinderResult) {
+                    Result.Add(XmlDeserializer.DeserializeFile(FilePath));
+                }
+            }
+            catch (Exception Ex)
+            {
+                Assert.Fail($"Finder.FindXmlFilesWithParsedClassesAndStructs function shouldn't throw any exceptions : {Ex.Message}");
+            }
+
+            var ExpectedCountOfClasses = 2;
+            Assert.AreEqual(ExpectedCountOfClasses, Result.Count);
+
+
+        }
+
+    }
+}
