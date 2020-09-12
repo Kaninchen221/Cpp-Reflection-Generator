17d66a30e97e731028644c1a913faced5fd52da1
Paweł Krolik <pawel8krolik@gmail.com>
2020-09-12 17:43:42 +0200

    Add more code

--- Cpp-Reflection-Generator-Tests/test_input/HeaderWithNormalCode.h
+++ Cpp-Reflection-Generator-Tests/test_input/HeaderWithNormalCode.h
@@ -1,9 +1,15 @@
 namespace zt {
 	
+	template<class T>
 	class NormalClass {
 		
 	public:
 		int testValue = 10;
+		
+		T objectOfTemplateParamType;
+		
+		template<class T>
+		void DoSomething(T t);
 		
 	};
 	
