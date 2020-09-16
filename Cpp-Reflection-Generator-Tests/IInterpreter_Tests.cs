1c53f986875da4dc3b8c5e63530a89aa76fcfeaa
Paweł Krolik <pawel8krolik@gmail.com>
2020-09-16 17:09:47 +0200

    Add IInterpreter interface.
    Interface who should help with remove recurancy.

---
+++ Cpp-Reflection-Generator-Tests/IInterpreter_Tests.cs
@@ -0,0 +1,57 @@
+﻿using Cpp_Reflection_Generator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace Cpp_Reflection_Generator_Tests
+{
+    [TestClass]
+    public class IInterpreter_Tests
+    {
+        class TestClass
+        {
+            public int X { get; set; } = 100;
+            public int Y { get; set; } = 200;
+        }
+
+        class InterpreterTestClass : IInterpreter
+        {
+            string IInterpreter.NameOfPropertyToInterpret { get => "X"; }
+            List<IInterpreter> IInterpreter.Interpreters { get => new List<IInterpreter>(); }
+
+            string IInterpreter.GenerateBeginOfThisSection(ref object ObjectToInterpret)
+            {
+                return $"Begin {IInterpreter.NewLine()}";
+            }
+
+            string IInterpreter.GenerateEndOfThisSection(ref object ObjectToInterpret)
+            {
+                return $"End {IInterpreter.NewLine()}";
+            }
+        }
+
+        [TestMethod]
+        public void GetProperties_Test()
+        {
+            object ObjToTest = new TestClass();
+            var Properties = IInterpreter.GetProperties(ref ObjToTest);
+
+            int ExpectedCount = 2;
+            Assert.AreEqual(ExpectedCount, Properties.Length);
+        }
+
+        [TestMethod]
+        public void IsMemberToInterpret_Test()
+        {
+            var TestClassObj = new TestClass();
+            var Type = TestClassObj.GetType();
+            var Property = Type.GetProperty("X");
+            var Interpreter = new InterpreterTestClass();
+
+            var IsToInterpret = IInterpreter.IsMemberToInterpret(Property, Interpreter);
+            Assert.IsTrue(IsToInterpret);
+        }
+    }
+}
---
+++ Cpp-Reflection-Generator/IInterpreter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Cpp_Reflection_Generator
+{
+    public interface IInterpreter
+    {
+        public string NameOfPropertyToInterpret { get; }
+        public List<IInterpreter> Interpreters { get; }
+
+        public string Interpret(ref object ObjectToInterpret)
+        {
+            var ResultOfInterpret = new StringBuilder();
+
+            var BeginOfThisSection = GenerateBeginOfThisSection(ref ObjectToInterpret);
+            ResultOfInterpret.Append(BeginOfThisSection);
+
+            var ResultOfPropagate = PropagateInterpreters(ref ObjectToInterpret);
+            ResultOfInterpret.Append(ResultOfPropagate);
+
+            var EndOfThisSection = GenerateEndOfThisSection(ref ObjectToInterpret);
+            ResultOfInterpret.Append(EndOfThisSection);
+
+            return ResultOfInterpret.ToString();
+        }
+
+        protected string GenerateBeginOfThisSection(ref object ObjectToInterpret);
+        protected string GenerateEndOfThisSection(ref object ObjectToInterpret);
+
+        static public char NewLine()
+        {
+            return '\n';
+        }
+
+        string PropagateInterpreters(ref object ObjectToInterpret)
+        {
+            var ResultOfPropagate = new StringBuilder();
+            var Properties = GetProperties(ref ObjectToInterpret);
+            
+            foreach(var Property in Properties)
+            {
+                foreach(var Interpreter in Interpreters)
+                {
+                    if (IsMemberToInterpret(Property, Interpreter))
+                    {
+                        var ValueOfProperty = Property.GetValue(ObjectToInterpret);
+                        var ResultOfInterpreter = Interpreter.Interpret(ref ValueOfProperty);
+                        ResultOfPropagate.Append(ResultOfInterpreter);
+                    }
+                }
+            }
+
+            return ResultOfPropagate.ToString();
+        }
+
+        static public System.Reflection.PropertyInfo[] GetProperties(ref object Object)
+        {
+            return Object.GetType().GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance);
+        }
+
+        static public bool IsMemberToInterpret(System.Reflection.PropertyInfo Property, IInterpreter Interpreter)
+        {
+            if (Property.Name.Equals(Interpreter.NameOfPropertyToInterpret))
+                return true;
+            else
+                return false;
+        }
+    }
+}
